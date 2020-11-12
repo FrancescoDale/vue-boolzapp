f856a1a6a0b2476c99e5245ed19f5bac803785d8
FrancescoDale <fd060862@gmail.com>
2020-11-12 15:40:38 +0100

    array con oggetti contatti e sottooggetti messaggi

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,31 @@
 var app = new Vue ({
     el : '#root',
     data : {
+        contacts : [
+            {
+                name : 'Gianni',
+                avatar :'',
+                visible : true,
+                messages : [
+                    {
+                        date: '12/11/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent',
+                    }
+                    {
+                        date: '12/11/2020 15:35:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent',
+                    }
+                    {
+                        date: '12/11/2020 15:45:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent',
+                    }
+                ],
+            }
 
+        ],
     },
     methods : {
 
